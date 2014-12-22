73cbe97e81280cbc564425c140164f1455c60744
Ed Jones <ed@errorstudio.co.uk>
2014-12-22 21:31:39 +0000

    Add a markdown helper

---
+++ app/helpers/markdown_helper.rb
@@ -0,0 +1,8 @@
+module MarkdownHelper
+  def parse_markdown(content)
+    @markdown ||= Redcarpet::Markdown.new(
+        Redcarpet::Render::HTML, autolink: true, space_after_headers: true, fenced_code_blocks: true
+    )
+    @markdown.render(content)
+  end
+end
