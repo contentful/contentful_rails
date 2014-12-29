ec87b4dfb4e56829be4e76eec5c9b8660c72bfa5
Ed Jones <ed@errorstudio.co.uk>
2014-12-29 23:20:48 +0000

    Add webhooks controller with debug method

---
+++ app/controllers/contentful_rails/webhooks_controller.rb
@@ -0,0 +1,11 @@
+class ContentfulRails::WebhooksController < ActionController::Base
+  if ContentfulRails.configuration.authenticate_webhooks
+    http_basic_authenticate_with  name: ContentfulRails.configuration.webhooks_username,
+                                  password: ContentfulRails.configuration.webhooks_password
+  end
+
+  def debug
+    render text: "Debug method works ok"
+  end
+
+end
