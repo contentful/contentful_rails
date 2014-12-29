176ef8cb639fbe0ad3a499a6d22217de68d1c56c
Ed Jones <ed@errorstudio.co.uk>
2014-12-29 23:20:30 +0000

    Make this a mountable engine

--- config/routes.rb
+++ config/routes.rb
@@ -1,2 +1,9 @@
-Rails.application.routes.draw do
+ContentfulRails::Engine.routes.draw do
+  #if ContentfulRails::Configuration.set_routes
+
+    scope constraints: ContentfulRails::DevelopmentConstraint do
+      get "/debug", to: "webhooks#debug"
+    end
+
+  #end
 end
--- lib/contentful_rails/engine.rb
+++ lib/contentful_rails/engine.rb
@@ -1,11 +1,25 @@
 module ContentfulRails
   class Engine < ::Rails::Engine
+
+    isolate_namespace ContentfulRails
+
+    #Iterate through all models which inherit from ContentfulModel::Base
+    #and add an entry mapping for them, so calls to the Contentful API return
+    #the appropriate classes.
     initializer "add_entry_mappings_for_contentful_models" do
       if defined?(ContentfulModel)
         Rails.application.eager_load!
         ContentfulModel::Base.descendents.each do |klass|
           klass.send(:add_entry_mapping)
         end
+      end
+    end
+
+    #if we're at the end of initialization and there's no config object,
+    #set one up with the default options (i.e. an empty proc)
+    config.after_initialize do
+      if ContentfulRails.configuration.nil?
+        ContentfulRails.configure {}
       end
     end
   end
