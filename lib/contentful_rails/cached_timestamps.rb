46e26c3dfe3fb1c72df5b90a4ff4c0e24cba34e6
Ed Jones <ed@errorstudio.co.uk>
2014-12-30 15:39:25 +0000

    Break timestamp_cache_key out into method on model

--- lib/contentful_rails/cached_timestamps.rb
+++ lib/contentful_rails/cached_timestamps.rb
@@ -3,18 +3,21 @@ module ContentfulRails
   #to check the cache for its timestamp before making an expensive API call
   module CachedTimestamps
     def self.included(base)
-      puts "Timestamps were included in #{base}"
       base.class_eval do
         alias_method_chain :updated_at, :caching
       end
     end
+
     def updated_at_with_caching
-      cache_key = "contentful_timestamp/#{self.class.content_type_id}/#{self.id}"
-      puts "#{cache_key}"
-      Rails.cache.fetch(cache_key) do
+      Rails.cache.fetch(self.timestamp_cache_key) do
         updated_at_without_caching
       end
     end
+
+    def timestamp_cache_key
+      "contentful_timestamp/#{self.class.content_type_id}/#{self.id}"
+    end
+
   end
 
 end
