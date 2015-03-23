530de6ef7abc8e83651e600753f7973b8ef83da3
Ed Jones <ed@errorstudio.co.uk>
2015-03-23 20:38:37 +0000

    Fix for silly module naming error

--- lib/contentful_rails/sluggable.rb
+++ lib/contentful_rails/sluggable.rb
@@ -1,19 +1,17 @@
 module ContentfulRails
   module Sluggable
+    extend ActiveSupport::Concern
     # A quick mixin to slugify a field other than 'slug'
     # NOTE that if you include sluggable (and define a slug field) in a class which responds to
     # slug() than you'll get a stack level too deep error.
-    module Sluggable
-      extend ActiveSupport::Concern
 
-      class_methods do
-        attr_accessor :slug_field
-      end
+    class_methods do
+      attr_accessor :slug_field
+    end
 
-      def slug
-        if self.class.slug_field.present?
-          self.send(self.class.slug_field).parameterize(Rails.configuration.x.param_delimiter)
-        end
+    def slug
+      if self.class.slug_field.present?
+        self.send(self.class.slug_field).parameterize(Rails.configuration.x.param_delimiter)
       end
     end
   end
