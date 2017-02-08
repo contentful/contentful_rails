2a500775b2866713592a2622b560850f7cf2369c
richardhessler <richessler@gmail.com>
2017-02-08 14:23:04 -0500

    updating content-type of json

--- lib/contentful_rails/engine.rb
+++ lib/contentful_rails/engine.rb
@@ -52,7 +52,7 @@ module ContentfulRails
 
     initializer "add_contentful_mime_type" do
       content_type = "application/vnd.contentful.management.v1+json"
-      Mime::Type.register "application/json", :contentful_json, [content_type]
+      Mime::Type.register content_type, :contentful_json, [content_type]
 
       ActionDispatch::ParamsParser::DEFAULT_PARSERS[Mime::Type.lookup(content_type)] = lambda do |body|
         JSON.parse(body)
