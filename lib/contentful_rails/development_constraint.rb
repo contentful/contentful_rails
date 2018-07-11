891f3a425e9030f12647b4b2c611454ada9b59ca
Reid Cooper <reid.cooper8@gmail.com>
2018-07-11 12:27:25 -0400

    Correct the regex syntax when determing if the request is in development env

--- lib/contentful_rails/development_constraint.rb
+++ lib/contentful_rails/development_constraint.rb
@@ -3,7 +3,7 @@ module ContentfulRails
   class DevelopmentConstraint
     # Returns if current request is coming from development environment or localhost
     def self.matches?(request)
-      Rails.env =~ %r{/development/} && (request.remote_ip =~ %r{/127.0.0/} || request.remote_ip =~ %r{/^::1$/})
+      Rails.env =~ %r{development} && (request.remote_ip =~ %r{127.0.0} || request.remote_ip =~ %r{^::1$})
     end
   end
 end
